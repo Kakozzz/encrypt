1b92234edb4be96440db05d10503bb409c64015b
Kakozzz <cumbajin2006@gmail.com>
2025-10-30 12:43:22 -0500

    Refactor xor_cifrar_desencriptar function

--- encriptatin.py
+++ encriptatin.py
@@ -2,11 +2,11 @@ import random
 def xor_cifrar_desencriptar(texto):
     clave = [random.randint(0, 1) for _ in range(len(texto))]
     cifrado = ""
-    desencriptado = ""
     for i in range(len(texto)):
-        c = chr(ord(texto[i]) ^ clave[i])  
-        cifrado += c
-        desencriptado += chr(ord(c) ^ clave[i])  
+        cifrado += chr(ord(texto[i]) ^ clave[i])
+    desencriptado = ""
+    for i in range(len(cifrado)):
+        desencriptado += chr(ord(cifrado[i]) ^ clave[i])
     return cifrado, desencriptado, clave
 texto = input("Ingrese el texto a cifrar: ")
 cifrado, desencriptado, clave = xor_cifrar_desencriptar(texto)
